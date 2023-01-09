fe161ff7a018d1727c47a2dd04a012c4bc59d85a
Roman Minaev <1@ali.m-rv.ru>
2023-01-09 08:54:47 +0300

    Second commit

--- price_test.js
+++ price_test.js
@@ -4,10 +4,12 @@ Scenario('test price in order', async ({ I }) => {
     I.amOnPage('https://www.reg.ru/web-sites/website-builder/');
     I.see('reg.ru');
     let hostingPrice = await I.grabValueFrom('//*[@id="content"]/div[2]/form/div/div[3]/div/div[2]/p/strong');
-    //return hostingPrice;
     I.click('//*[@id="content"]/div[2]/form/div/div[3]/div/div[3]/div');
     I.fillField('domain', 'lets-reg-test-domain.ru');
-    I.see('199');
-    const domainPrice = await I.grabTextFrom('');
+    I.click('//*/div/div[3]/div[1]/div/div[2]/div[4]/div/div/div/div/div[2]/div[2]/div/div/div/div[1]/div');
+    I.see('Оплатить');
+    //let domainPrice = await I.grabValueFrom('//span[@class = "domain-in-order-price__final"]');
+    //let orderPrice = await I.grabValueFrom('//span[@class="order-footer-price__price"]');
+
 
 });
